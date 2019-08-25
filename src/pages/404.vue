af7c2344471f2e6da3b69d66a8c9391734bd66fa
jradtilbrook <jrad.tilbrook@gmail.com>
2019-08-25 11:33:14 +0800

    Add alt tags to images

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,7 +5,7 @@
         <h2 class="text-6xl font-bold mb-4">404</h2>
         <h2 class="text-xl mb-8">Page Not Found</h2>
         <g-link class="text-blue-700 text-xl hover:text-blue-900" to="/">Go Home</g-link>
-        <g-image class="mx-auto my-16 rounded-full" width="500" src="../../static/taken_blue_600.svg"/>
+        <g-image class="mx-auto my-16 rounded-full" width="500" src="../../static/taken_blue_600.svg" alt="Alien ufo"/>
       </div>
     </div>
   </Layout>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <Layout className="flex flex-col justify-center">
     <div class="container mx-auto items-center">
 
-      <g-image class="mx-auto my-16" width="500" src="../../static/under_construction_blue_600.svg"/>
+      <g-image class="mx-auto my-16" width="500" src="../../static/under_construction_blue_600.svg" alt="House under construction"/>
       <h1 class="text-3xl text-center font-bold mb-16">Still under construction ...</h1>
 
     </div>
